0adf83dd22a8fc943f70e893af9119f63a5800d4
ivo <ivo.ivo@ivo.com>
2016-10-13 22:57:34 +0200

    add routing

--- test/app/app.module.ts
+++ test/app/app.module.ts
@@ -1,16 +1,38 @@
-import {NgModule }      from '@angular/core';
-import {BrowserModule } from '@angular/platform-browser';
+import {BrowserModule}  from '@angular/platform-browser';
 import {FormsModule}    from '@angular/forms';
+import {NgModule}       from '@angular/core';
+import {RouterModule}   from '@angular/router';
 
-import {AppComponent}    from "./app.component"
+import {AppComponent}   from "./app.component";
+import {EchoComponent}  from "./routes/echo/echo.component";
+import {LoginComponent} from "./routes/login/login.component";
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'echo',
+        component: EchoComponent
+      },
+      {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+      },
+    ])
   ],
   declarations: [ 
-    AppComponent 
+    AppComponent,
+    EchoComponent,
+    LoginComponent 
+  ],
+  providers: [
   ],
   bootstrap:    [ 
     AppComponent 
---
+++ test/app/routes/echo/echo.component.ts
@@ -0,0 +1,62 @@
+import {Component }        from '@angular/core';
+import {Observable}        from 'rxjs/Observable';
+
+import {WebsocketService } from './../../services/websocket.service';
+
+@Component({
+  moduleId   : module.id,
+  selector   : 'test-app',
+  providers  : [WebsocketService],
+  templateUrl: 'echo.component.html'
+})
+export class EchoComponent { 
+    private m_WebsocketDataIn : Observable<any>;
+    private m_WebsocketDatas  : any[] = [];
+    private m_Type            : string;
+    private m_Message         : string;
+
+    public constructor(private m_WebsocketService : WebsocketService) {
+        //this.m_WebsocketService.connect("ws://echo.websocket.org/");
+        this.m_WebsocketService.connect("ws://192.168.0.69:8000/echo");
+
+        this.m_WebsocketService.getObservable().subscribe(
+            result => {
+                this.m_WebsocketDatas.push(result);
+            }, 
+            error => {
+                console.log("Error result");
+            }
+        );
+        this.m_WebsocketDataIn = m_WebsocketService.getObservable();
+
+        var data1 = {
+            type: "type",
+            message: "message 1"
+        };
+        var data2 = {
+            type: "type",
+            message: "message 2"
+        };
+        var data3 = {
+            type: "type",
+            message: "message 3"
+        };
+        this.m_WebsocketService.send(data1);
+        this.m_WebsocketService.send(data2);
+        this.m_WebsocketService.send(data3);
+    }
+
+    public sendMessage() {
+        var data = {
+            type: this.m_Type,
+            message: this.m_Message
+        };
+        this.m_WebsocketService.send(data);
+        this.clearMessage();
+    }
+
+    public clearMessage() {
+        this.m_Type    = "";
+        this.m_Message = "";
+    }
+}
