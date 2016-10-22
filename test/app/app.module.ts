695395569e7195583fda7b7759a26805319bc815
ivo <ivo.ivo@ivo.com>
2016-10-22 20:58:42 +0200

    beautify

--- test/app/app.module.ts
+++ test/app/app.module.ts
@@ -1,21 +1,28 @@
+//Built-in modules
 import {BrowserModule}         from '@angular/platform-browser';
 import {FormsModule}           from '@angular/forms';
 import {NgModule}              from '@angular/core';
 import {RouterModule}          from '@angular/router';
 
-import {AppComponent}          from "./app.component";
-
+//Own services
 import {UserService}           from './services/user.service';
 import {WebsocketUserService}  from './services/websocket.user.service';
 
+//Own components: application
+import {AppComponent}          from "./app.component";
+
+//Own components: routes
 import {EchoComponent}         from "./routes/echo/echo.component";
 import {SimpleButtonComponent} from "./routes/simple-button/simple-button.component";
 import {WelcomeComponent}      from "./routes/welcome/welcome.component";
 
+//Own components: components
 import {LoginComponent}        from "./components/login/login.component";
 import {StatusBarComponent}    from "./components/status-bar/status-bar.component";
 
+//Module definition
 @NgModule({
+  //Inject built-in modules
   imports: [
     BrowserModule, 
     FormsModule,
@@ -39,10 +46,12 @@ import {StatusBarComponent}    from "./components/status-bar/status-bar.componen
       },
     ])
   ],
+  //Inject your own services
   providers: [
     WebsocketUserService,
     UserService,
   ],
+  //Inject your own components
   declarations: [ 
     //application
     AppComponent,
@@ -54,6 +63,7 @@ import {StatusBarComponent}    from "./components/status-bar/status-bar.componen
     LoginComponent,
     StatusBarComponent
   ],
+  //Module you need to bootstrap
   bootstrap:    [ 
     AppComponent 
   ]
