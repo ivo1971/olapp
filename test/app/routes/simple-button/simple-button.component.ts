2ec19b6390139562e5466b3002473afd66dd936b
ivo <ivo.ivo@ivo.com>
2016-10-27 20:54:47 +0200

    get button data from the server

--- test/app/routes/simple-button/simple-button.component.ts
+++ test/app/routes/simple-button/simple-button.component.ts
@@ -1,9 +1,6 @@
-import {BehaviorSubject}      from 'rxjs/BehaviorSubject';
 import {Component}            from '@angular/core';
-import {Input}                from '@angular/core';
 import {Observable}           from 'rxjs/Observable';
 import {OnInit}               from '@angular/core';
-import {TimerObservable}      from 'rxjs/Observable/TimerObservable';
 
 import {ComponentBase}        from './../../classes/component-base.class';
 import {SimpleButtonInfo}     from './../../classes/simple-button-info.class';
@@ -24,28 +21,12 @@ export class SimpleButtonComponent extends ComponentBase implements OnInit {
     }
 
     public ngOnInit() : void {
+        //inform parent
         this.sendLocation("simple-button");
 
-        this._info.background = "info";
-        this._info.pressed    = false;
-        this._subjectInfo.next(this._info);   
-
-        let timer = TimerObservable.create(5000,2000);
-        timer.subscribe(t=> {
-            this._count++;
-            let simpleButtonTeamInfo : SimpleButtonTeamInfo = new SimpleButtonTeamInfo();
-            simpleButtonTeamInfo.name       = "name [" + this._count + "]";
-            simpleButtonTeamInfo.background = "danger";
-            simpleButtonTeamInfo.members    = ['ivo', 'bo', 'bart', 'geert'];
-            this._info.teams.push(simpleButtonTeamInfo); 
-            this._info.background = "warning";
-            this._info.pressed    = !this._info.pressed;
-            this._subjectInfo.next(this._info);   
-        });        
+        //register routing MI
+        this.observableInfo = this._websocketService.register("simple-button");
     }
 
-    private _count          : number                            = 0;
-    private _info           : SimpleButtonInfo                  = new SimpleButtonInfo();
-    private _subjectInfo    : BehaviorSubject<SimpleButtonInfo> = new BehaviorSubject<SimpleButtonInfo>(this._info);
-    private observableInfo  : Observable<SimpleButtonInfo>      = this._subjectInfo.asObservable();
+    private observableInfo  : Observable<SimpleButtonInfo>;
 }
