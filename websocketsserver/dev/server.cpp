d4952a5df1d640e2be4c1be4ef45483f97bb474e
ivo1971 <ivo.luyckx@edpnet.be>
2016-11-01 10:56:22 +0100

    add libcurl

--- websocketsserver/dev/server.cpp
+++ websocketsserver/dev/server.cpp
@@ -11,13 +11,29 @@
 using namespace std;
 using namespace seasocks;
 
-int main(int /*argc*/, const char* /*argv*/[]) {
+int main(int argc, const char* argv[]) {
+  //parse arguments
+  std::string ipAddressServer;
+  {
+    bool addrIsNext = false;
+    for(int i = 0 ; i < argc ; ++i) {
+      if(addrIsNext) {
+	ipAddressServer = std::string(argv[i]);
+      }
+      addrIsNext = (0 == strcmp("--address", argv[i]));
+      }
+    fprintf(stdout, "local address: %s\n", ipAddressServer.c_str());
+    if(0 == ipAddressServer.length()) {
+      fprintf(stderr, "no local address configured\n");
+      exit(-1);
+    }
+  }
+
   //construction
   shared_ptr<Logger>               spLogger             (new PrintfLogger(Logger::DEBUG));
   shared_ptr<Server>               spServer             (new Server(spLogger));
   shared_ptr<CWsQuizHandler>       spWsQuizHandler      (new CWsQuizHandler(spLogger, spServer));
   shared_ptr<CQuizManager>         spQuizManger         (new CQuizManager(spLogger, spWsQuizHandler));
-  const std::string                ipAddressServer      ("ab.cd.ef.gh");
   CConfigureServer                 configureServer      (ipAddressServer);
 
   //configuration
