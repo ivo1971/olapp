5132337ee3dc23b9045eb1206283aa87f86f9418
ivo <ivo.ivo@ivo.com>
2017-01-17 20:34:22 +0100

    add team

--- olapp/www/app/routes/configure-teams-master/configure-teams-master.component.ts
+++ olapp/www/app/routes/configure-teams-master/configure-teams-master.component.ts
@@ -14,6 +14,8 @@ import {WebsocketUserService}  from './../../services/websocket.user.service';
     templateUrl: 'configure-teams-master.component.html'
 })
 export class ConfigureTeamsMasterComponent extends ComponentBase implements OnInit { 
+    private newTeamName : string = "";
+
     /* Construction
      */
     public constructor(
@@ -25,6 +27,38 @@ export class ConfigureTeamsMasterComponent extends ComponentBase implements OnIn
     /* Life-cycle hooks
      */
     public ngOnInit() : void {
+    }
+
+    /* Template event handlers
+     */
+    private onClickAddTeam(value: string) : void {
+        //send the team name to the websocket
+        this._websocketUserService.sendMsg("team-add", {
+            teamId: this.getNewGUIDString(),
+            teamName: this.newTeamName;
+        });
+
+        //clear edit box
+        this.newTeamName = "";
+        //TODO:
+        //- sent to websocket
+        //- generate a team ID, required to rename later
+    }
+
+    /* Help functions
+     */
+    private getNewGUIDString() {
+        // your favourite guid generation function could go here
+        // ex: http://stackoverflow.com/a/8809472/188246
+        let d = new Date().getTime();
+        if (window.performance && typeof window.performance.now === "function") {
+            d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            let r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
     }
 }
  
