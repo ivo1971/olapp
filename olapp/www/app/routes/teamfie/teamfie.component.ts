3285d7b957199e329833465ff0cc6ecbd1a89f02
ivo <ivo.ivo@ivo.com>
2017-02-02 21:36:03 +0100

    improve teamfie

--- olapp/www/app/routes/teamfie/teamfie.component.ts
+++ olapp/www/app/routes/teamfie/teamfie.component.ts
@@ -24,6 +24,7 @@ import {WebsocketUserService} from './../../services/websocket.user.service';
 export class TeamfieComponent extends ComponentBase implements OnInit, OnDestroy { 
     private imageContent      : string = "";
     private imageContentValid : boolean = false;
+    private teamName          : string  = "";
 
     public constructor(
         private changeDetectorRef     : ChangeDetectorRef,
@@ -39,35 +40,44 @@ export class TeamfieComponent extends ComponentBase implements OnInit, OnDestroy
     public ngOnDestroy() : void {
     }
 
+    private onClickTakeTeamfie() : void {
+        console.log("onClickTakeTeamfie in");
+        var srcType = Camera.PictureSourceType.CAMERA;
+        var options = this.cameraSetOptions(srcType);
+        navigator.camera.getPicture(image => {
+            console.log("onClickTakeTeamfie OK [" + image.length + "]");
+            this.imageContent      = image;
+            this.imageContentValid = true;
+            this.changeDetectorRef.detectChanges(); //trigger Angular digest cycle manually
+        }, function cameraError(error) {
+            console.log("onClickTakeTeamfie failed: " + error);
+            this.imageContentValid = false;
+            this.changeDetectorRef.detectChanges(); //trigger Angular digest cycle manually
+        }, options);
+        console.log("onClickTakeTeamfie out");
+    }
+
+    private onClickSubmit() : void {
+        console.log("onClickSubmit [" + this.imageContent.length + "]");
+        this._websocketUserService.sendMsg("teamfie", {
+            name: this.teamName,
+            image: this.imageContent;
+        });
+    }
+
     private cameraSetOptions(srcType: string) {
         var options = {
             // Some common settings are 20, 50, and 100
-            quality: 50,
+            quality: 20,
             destinationType: Camera.DestinationType.DATA_URL,
             // In this app, dynamically set the picture source, Camera or photo gallery
             sourceType: srcType,
             encodingType: Camera.EncodingType.JPEG,
             mediaType: Camera.MediaType.PICTURE,
             allowEdit: true,
+            cameraDirection: Camera.Direction.BACK,
             correctOrientation: true  //Corrects Android orientation quirks
         }
         return options;
-    }
-
-    private onClickTakeTeamfie() : void {
-        console.log("onClickTakeTeamfie in");
-        var srcType = Camera.PictureSourceType.CAMERA;
-        var options = this.cameraSetOptions(srcType);
-        navigator.camera.getPicture(image => {
-            console.log("onClickTakeTeamfie OK");
-            this.imageContent      = image;
-            this.imageContentValid = true;
-            this.changeDetectorRef.detectChanges();
-        }, function cameraError(error) {
-            console.log("onClickTakeTeamfie failed: " + error);
-            this.imageContentValid = false;
-            this.changeDetectorRef.detectChanges();
-        }, options);
-        console.log("onClickTakeTeamfie out");
     }
 }
