baf5ce885a6d2190c44073103eed158008d81060
ivo <ivo.ivo@ivo.com>
2017-03-11 10:29:02 +0100

    questions development

--- olapp/www/app/routes/questions/questions-master.component.ts
+++ olapp/www/app/routes/questions/questions-master.component.ts
@@ -23,7 +23,9 @@ export class QuestionsMasterComponent extends ComponentBase implements OnInit, O
     /* Private variables intended for the template
      * (hence at the top)
      */
-    private numberOfQuestions  : number    = 10;
+    private numberOfQuestions  : number  = 10;
+    private resetConfirm       : boolean = false;
+    private modeAnswering      : boolean = true;
 
     /* Construction
      */
@@ -42,6 +44,24 @@ export class QuestionsMasterComponent extends ComponentBase implements OnInit, O
     /* Life-cycle hooks
      */
     public ngOnInit() : void {
+        this.observableQuestionsConfigure = this._websocketUserService
+                                             .register("questions-configure-master")
+        this.observableQuestionsConfigureSubscription = this.observableQuestionsConfigure.subscribe(
+            data => {
+                console.log(data);
+                this.numberOfQuestions = data["nbrOfQuestions"]; 
+                console.log("observableQuestionsConfigureSubscription [" + this.numberOfQuestions + "]");
+            }
+        );
+
+        this.observableQuestionsAction = this._websocketUserService
+                                             .register("questions-action-master")
+        this.observableQuestionsActionSubscription = this.observableQuestionsAction.subscribe(
+            data => {
+                this.modeAnswering = data["answering"]; 
+                console.log("observableQuestionsActionSubscription [" + this.modeAnswering + "]");
+            }
+        );
     }
 
     public ngOnDestroy() : void {
@@ -49,16 +69,26 @@ export class QuestionsMasterComponent extends ComponentBase implements OnInit, O
 
     /* Event handlers called from the template
      */
-    private onClickSetNumberOfQuestions() : void {
+    private onClickSetNumberOfQuestionsOne() : void {
+        this.resetConfirm = true;
+    }
+
+    private onClickSetNumberOfQuestionsCancel() : void {
+        this.resetConfirm = false;
+    }
+
+    private onClickSetNumberOfQuestionsTwo() : void {
+        this.resetConfirm = false;
         this._websocketUserService.sendMsg("questions-configure", {
             nbrOfQuestions: this.numberOfQuestions
         });    
         this.onClickRadioAction(true);
-        //TODO: set radio button to 'answering' in the UI
+        this.modeAnswering = true;
     }
 
     private onClickRadioAction(answering : boolean) : void {
         console.log("onClickRadioAction: [" + answering + "]");
+        this.modeAnswering = answering;
         this._websocketUserService.sendMsg("questions-action", {
             answering: answering
         });        
@@ -69,4 +99,8 @@ export class QuestionsMasterComponent extends ComponentBase implements OnInit, O
 
     /* Private members
      */
+    private observableQuestionsConfigure                   : Observable<any>;
+    private observableQuestionsConfigureSubscription       : Subscription;
+    private observableQuestionsAction                      : Observable<any>;
+    private observableQuestionsActionSubscription          : Subscription;
 }
